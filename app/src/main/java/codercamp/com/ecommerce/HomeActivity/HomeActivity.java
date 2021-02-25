b79ae85898f6492ef47577bfe4528d02f2de230a
Md. Shamim Hossain <mdshamimh529@gmail.com>
2021-02-25 13:33:46 +0600

    Initial commit

---
+++ app/src/main/java/codercamp/com/ecommerce/HomeActivity/HomeActivity.java
@@ -0,0 +1,16 @@
+package codercamp.com.ecommerce.HomeActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import codercamp.com.ecommerce.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
---
+++ app/src/main/java/codercamp/com/ecommerce/Prevalent/Prevalent.java
@@ -0,0 +1,10 @@
+package codercamp.com.ecommerce.Prevalent;
+
+import codercamp.com.ecommerce.Model.Users;
+
+public class Prevalent {
+    public static Users currentOnlineUser;
+
+    public static final String UserPhoneKey = "UserPhone";
+    public static final String UserPasswordKey = "UserPassword";
+}
